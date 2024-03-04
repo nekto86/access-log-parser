882feae646f9f861d21328e12a7735f4ef5f9fa4
nekto86 <rinat.mux@yandex.ru>
2024-03-04 14:44:07 +0500

    Добавлена логика проверки верно указанного файла пользователем, а также сообщения на случай неверно указанного пути

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число");
+      /*  System.out.println("Введите первое число");
         int firstNumber = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число");
         int secondNumber = new Scanner(System.in).nextInt();
@@ -11,5 +12,25 @@ public class Main {
                 "Их разность: " + (firstNumber - secondNumber)  + "\n" +
                 "Их произведение: " + firstNumber * secondNumber + "\n" +
                 "Их частное: " + (double)firstNumber/secondNumber);
+
+                */
+        boolean isFileExist, isDirectory;
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            isFileExist = file.exists();
+            isDirectory = file.isDirectory();
+            if (isFileExist && !isDirectory){
+                count++;
+                System.out.println("Путь указан верно\nЭто " + count + " верно указанный файл");
+                continue;
+            }
+            if (isFileExist) {
+                System.out.println("Указан путь не к файлу, а к папке\nПерепроверьте и попробуйте ещё раз");
+                continue;
+            }
+            System.out.println("Указанный путь не существует\nПерепроверьте и попробуйте ещё раз");
+        }
     }
 }
